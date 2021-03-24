df4ae58c3120825c10d9d1127b508c484a83466c
CristhianCC <cristhianmcm12@gmail.com>
2021-03-24 17:42:42 +0100

    Added getters and setter. Closes #1.

--- src/main/java/com/spring/framework/pet/clinic/model/Pet.java
+++ src/main/java/com/spring/framework/pet/clinic/model/Pet.java
@@ -9,4 +9,36 @@ public class Pet {
     private Owner owner;
     private PetType petType;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
 }
--- src/main/java/com/spring/framework/pet/clinic/model/PetType.java
+++ src/main/java/com/spring/framework/pet/clinic/model/PetType.java
@@ -1,10 +1,5 @@
 package com.spring.framework.pet.clinic.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class PetType {
     private String name;
 }
