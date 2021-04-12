2e0dde7e494850b17fbdbe1f038cb0fa13e46963
CristhianCC <cristhianmcm12@gmail.com>
2021-04-12 15:57:51 +0200

    Removed Imports.

--- pet-clinic-data/src/main/java/com/spring/framework/pet/clinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/spring/framework/pet/clinic/services/PetService.java
@@ -2,7 +2,5 @@ package com.spring.framework.pet.clinic.services;
 
 import com.spring.framework.pet.clinic.model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet, Long>{
 }
